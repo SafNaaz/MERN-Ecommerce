8eb3ff6703370d6e0eeb1347e19bf3c009c1c61f
Safnas <iamsafnas@gmail.com>
2020-10-14 20:40:07 +0530

    added header,footer skeleton and bootswatch css

--- proshop/frontend/src/App.js
+++ proshop/frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
+      <Header/>
       <h1>Welcome to ProShop</h1>
+      <Footer/>
     </>
   );
 }

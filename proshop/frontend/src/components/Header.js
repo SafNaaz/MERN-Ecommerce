c8722ca3f060402499eab0a4a374adab42ff131b
Safnas <iamsafnas@gmail.com>
2020-10-14 21:12:23 +0530

    installed rct-btstrp , then nav, header, footer, font awesome and some css

--- proshop/frontend/src/components/Header.js
+++ proshop/frontend/src/components/Header.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <header>
-            header
-        </header>
-    )
-}
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>{/* Moves brand etc in a proper width*/}
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto"> {/* Moves cart etc to left*/}
+              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-export default Header
+export default Header;

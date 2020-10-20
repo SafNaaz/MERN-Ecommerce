5d3d8ae07a8d4a66a79b59721d0170acffe3d433
Safnas <iamsafnas@gmail.com>
2020-10-20 23:08:14 +0530

    taking user without password using jwt and handling exception

--- proshop/backend/middleware/authMiddleware.js
+++ proshop/backend/middleware/authMiddleware.js
@@ -9,10 +9,13 @@ const protect = asyncHandler( async (req,res,next) =>{
         try {
             token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            console.log(decoded)
+
+            req.user = await (await User.findById(decoded.id)).isSelected('-password')
             next()
         } catch (error) {
-            
+            console.error(error)
+            res.status(401)
+            throw new Error('Not authorized, token failed')
         }
     }
 

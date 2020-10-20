787b61984681b590a4b6756e07daae7c82562ead
Safnas <iamsafnas@gmail.com>
2020-10-20 18:12:16 +0530

    showing subtotal using reduce function

--- proshop/frontend/src/screens/CartScreen.js
+++ proshop/frontend/src/screens/CartScreen.js
@@ -83,8 +83,15 @@ const CartScreen = ({ match, location, history }) => {
           </ListGroup>
         )}
       </Col>
-      <Col md={2}></Col>
-      <Col md={2}></Col>
+      <Col md={4}>
+          <Card>
+              <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                      <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items </h2>
+                  </ListGroup.Item>
+              </ListGroup>
+          </Card>
+      </Col>
     </Row>
   );
 };

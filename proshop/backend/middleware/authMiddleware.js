3dce9a1c59565d90307f02a7877c15bdb38e8fff
Safnas <iamsafnas@gmail.com>
2020-10-20 23:05:04 +0530

    checking decoded token * thrwoing error if no token is passed

--- proshop/backend/middleware/authMiddleware.js
+++ proshop/backend/middleware/authMiddleware.js
@@ -1,12 +1,26 @@
 const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
-const protect = async (req,res,next) =>{
+const protect = asyncHandler( async (req,res,next) =>{
     let token
 
-    console.log(req.headers.authorization)
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            console.log(decoded)
+            next()
+        } catch (error) {
+            
+        }
+    }
 
-    next()
-}
+    if(!token){
+        res.status(401)
+        throw new Error('Not authorized, no token')
+    }
+
+})
 
 module.exports = protect

375874b2cc822559cb6f34ee973aff5c8fdd379b
Safnas <iamsafnas@gmail.com>
2020-10-21 01:31:57 +0530

    cleaner code for cart

--- proshop/frontend/src/screens/CartScreen.js
+++ proshop/frontend/src/screens/CartScreen.js
@@ -14,22 +14,11 @@ import Message from "../components/Message";
 import { addToCart, removeFromCart } from "../actions/cartActions";
 
 const CartScreen = ({ match, location, history }) => {
-  const productId = match.params.id;
-
-  const qty = location.search
-    ? Number(location.search.split("=")[1])
-    : 1; /* anything after ?*/
 
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-
-  useEffect(() => {
-    if (productId) {
-      dispatch(addToCart(productId, qty));
-    }
-  }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
     dispatch(removeFromCart(id))

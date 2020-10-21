638fd3f06e7f283b3a810346eb565189fa72860c
Safnas <iamsafnas@gmail.com>
2020-10-21 13:03:26 +0530

    Product Screen changes

--- proshop/frontend/src/screens/ProductScreen.js
+++ proshop/frontend/src/screens/ProductScreen.js
@@ -32,17 +32,15 @@ const ProductScreen = ({ history, match }) => {
     match,
   ]); /* pass array with dependent values or don't pass here, match params */
 
-  const addToCartHandler = () =>{
-    dispatch(addToCart(product._id, qty))
-    history.push('/cart')
-  }
+  const addToCartHandler = () => {
+    dispatch(addToCart(product._id, qty));
+    history.push("/cart");
+  };
 
   return (
     <>
-    <h1>{product._id === match.params.id && product.name}</h1>
-      <Link className="btn btn-light my-3" to="/">
-        Go Back
-      </Link>
+      <h1>{product._id === match.params.id && product.name}</h1>
+
       {loading ? (
         <Loader />
       ) : error ? (
@@ -50,89 +48,98 @@ const ProductScreen = ({ history, match }) => {
       ) : product._id !== match.params.id ? (
         <Loader />
       ) : (
-        <Row>
-          <Col md={6}>
-            <Image src={product.image} alt={product.name} fluid />{" "}
-            {/* fluid to make image inside container */}
-          </Col>
-          <Col md={3}>
-            <ListGroup variant="flush">
-              {/* flush - without borders */}
-              {/* <ListGroup.Item>
+        <>
+          <Link className="btn btn-light my-3" to="/">
+            Go Back
+          </Link>
+          <Row>
+            <Col md={6}>
+              <Image src={product.image} alt={product.name} fluid />{" "}
+              {/* fluid to make image inside container */}
+            </Col>
+            <Col md={3}>
+              <ListGroup variant="flush">
+                {/* flush - without borders */}
+                {/* <ListGroup.Item>
                 <h3>{product.name}</h3>
               </ListGroup.Item> */}
-              <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
-              <ListGroup.Item>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
-              </ListGroup.Item>
-              
-              <ListGroup.Item>
-                Description : {product.description}
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-          <Col md={3}>
-            <Card>
-              <ListGroup variant="flush">
+                <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
                 <ListGroup.Item>
-                  <Row>
-                    <Col>Price:</Col>
-                    <Col>
-                      <strong>${product.price}</strong>
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Status:</Col>
-                    <Col>
-                      {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-                    </Col>
-                  </Row>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
                 </ListGroup.Item>
 
-                {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  Description : {product.description}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <ListGroup variant="flush">
                   <ListGroup.Item>
                     <Row>
-                      <Col>Qty</Col>
+                      <Col>Price:</Col>
                       <Col>
-                        <Form.Control
-                          as="select"
-                          value={qty}
-                          onChange={(e) => {
-                            setQty(e.target.value);
-                          }}
-                        >
-                          {[...Array(product.countInStock).keys()].map((x) => (
-                            <option key={x + 1} value={x + 1}>{x + 1}</option>
-                          ))}
-                        </Form.Control>
+                        <strong>${product.price}</strong>
                       </Col>
                     </Row>
                   </ListGroup.Item>
-                )}
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
 
-                <ListGroup.Item>
-                  <Button
-                    onClick={addToCartHandler}
-                    className="btn-block"
-                    type="button"
-                    disabled={product.countInStock === 0}
-                  >
-                    Add To Cart
-                  </Button>
-                </ListGroup.Item>
-              </ListGroup>
-            </Card>
-          </Col>
-        </Row>
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => {
+                              setQty(e.target.value);
+                            }}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
+
+                  <ListGroup.Item>
+                    <Button
+                      onClick={addToCartHandler}
+                      className="btn-block"
+                      type="button"
+                      disabled={product.countInStock === 0}
+                    >
+                      Add To Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+          <Link className="btn btn-light my-3" to="/">
+            Go Back
+          </Link>
+        </>
       )}
-      <Link className="btn btn-light my-3" to="/">
-        Go Back
-      </Link>
     </>
   );
 };

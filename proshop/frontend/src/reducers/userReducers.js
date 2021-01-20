e2569850cc7e1ea1d8102ee5f821d3e68891d3d5
Safnas <iamsafnas@gmail.com>
2021-01-20 19:58:12 +0530

    loading login screen after login fail

--- proshop/frontend/src/reducers/userReducers.js
+++ proshop/frontend/src/reducers/userReducers.js
@@ -19,7 +19,7 @@ import {
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return { loading: true, ...state };
+      return { loading: true};
     case USER_LOGIN_SUCCESS:
       return { loading: false, userInfo: action.payload };
     case USER_LOGIN_FAIL:
